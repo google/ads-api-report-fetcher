48da5c90e8b9b2bb026e3c9ebfec65ef081f6a6e
Andrei Markin <amarkin@google.com>
2022-02-16 13:58:47 +0300

    Update setup.py for PyPI upload
    Change-Id: I4b92694afad06d6e3ac9aa0096267a6632dbe381

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
+import pathlib
 from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
 
 setup(name="ads-api-reports-fetcher",
       version="0.1",
+      description="Library for fetching reports from Google Ads API and saving them locally / BigQuery.",
+      long_description=README,
+      long_description_content_type="text/markdown",
+      url="https://github.com/google/ads-api-reports-fetcher",
+      author="gPS Team",
+      author_email="no-reply@google.com",
+      license="Apache 2.0",
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Intended Audience :: Developers",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Operating System :: OS Independent",
+          "License :: OSI Approved :: Apache Software License"
+      ],
       packages=find_packages(include=["runner", "runner.*"]),
       install_requires=[
           "google-ads==14.1.0", "google-cloud-bigquery==2.26.0",
