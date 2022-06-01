acca1b993229378dabe93b829ccb5ab112773bf5
Andrei Markin <amarkin@google.com>
2022-06-01 13:27:01 +0400

    Update documentation
    * Update version of `google-ads` to 16.0.0 version in `setup.py` and `requirements.txt`
    * Update pip instructions in README
    
    Change-Id: I8e64a81b2a59bacb02d92712660bf4e832a4e79e

--- py/setup.py
+++ py/setup.py
@@ -5,32 +5,34 @@ HERE = pathlib.Path(__file__).parent
 
 README = (HERE / "README.md").read_text()
 
-setup(name="google-ads-api-report-fetcher",
-      version="0.1",
-      description="Library for fetching reports from Google Ads API and saving them locally / BigQuery.",
-      long_description=README,
-      long_description_content_type="text/markdown",
-      url="https://github.com/google/ads-api-reports-fetcher",
-      author="Google Inc. (gTech gPS CSE team)",
-      author_email="no-reply@google.com",
-      license="Apache 2.0",
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "Intended Audience :: Developers",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-          "Operating System :: OS Independent",
-          "License :: OSI Approved :: Apache Software License"
-      ],
-      packages=find_packages(include=["gaarf", "gaarf.*"]),
-      install_requires=[
-          "google-ads==14.1.0", "google-cloud-bigquery==2.26.0",
-          "pandas==1.3.4", "pyarrow==6.0.1", "tabulate"
-      ],
-      setup_requires=["pytest-runner"],
-      tests_requires=["pytest"],
-      entry_points={
-          "console_scripts": [
-              "gaarf=gaarf.fetcher:main",
-              "gaarf-postprocess=gaarf.post_processor:main",
-          ]
-      })
+setup(
+    name="google-ads-api-report-fetcher",
+    version="0.1",
+    description=
+    "Library for fetching reports from Google Ads API and saving them locally / BigQuery.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/google/ads-api-reports-fetcher",
+    author="Google Inc. (gTech gPS CSE team)",
+    author_email="no-reply@google.com",
+    license="Apache 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License"
+    ],
+    packages=find_packages(include=["gaarf", "gaarf.*"]),
+    install_requires=[
+        "google-ads==16.0.0", "google-cloud-bigquery==2.26.0", "pandas==1.3.4",
+        "pyarrow==6.0.1", "tabulate", "smart_open"
+    ],
+    setup_requires=["pytest-runner"],
+    tests_requires=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "gaarf=gaarf.fetcher:main",
+            "gaarf-postprocess=gaarf.post_processor:main",
+        ]
+    })
