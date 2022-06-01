0620ac919aa3da5e320a661ce372c0d9394d089b
Andrei Markin <amarkin@google.com>
2022-06-01 13:26:02 +0400

    Update fetcher to support API version
    Changes in api_clients.py:
    * Rename ga_service to ads_service
    * Remove confusing get_client method
    
    Changes in fetcher.py:
    * Simplify process_query function:
      * Add reader support
      * Rely on AdsQueryExecutor to handle query execution (rather than calling `search_stream` method from api client)
      * Make sure that args for query can be passed as a Dict and not argparse.Namespace)
      * Remove getters and parser from function to handle it by AdsQueryExecutor instance
    * Rely on AdsQueryExecutor to perform MCC expansion (rather then relying on directly calling `search_stream` method
    * Add --api-version and update --ads-config  cli flags
    * Clean up based on PEP8 linter recommendations
    
    Change-Id: Ibaf833bc90df3d8c7de493f9d431cece641846f5

--- py/gaarf/fetcher.py
+++ py/gaarf/fetcher.py
@@ -13,71 +13,45 @@
 # limitations under the License.
 
 from concurrent import futures
-from typing import Any, Callable, Dict, Sequence
+from typing import Any, Dict, List
 import sys
 import argparse
 from pathlib import Path
 import logging
+import traceback
 
-from google.ads.googleads.v9.services.services.google_ads_service.client import GoogleAdsServiceClient  #type: ignore
-from google.ads.googleads.errors import GoogleAdsException  #type: ignore
-from operator import attrgetter
+from google.ads.googleads.errors import GoogleAdsException  # type: ignore
 
-from . import parsers
-from . import writer
-from . import api_handler
 from . import api_clients
-from .query_editor import AdsQueryEditor
+from .io import writer, reader  # type: ignore
+from .query_executor import AdsQueryExecutor
 
 
-def process_query(query: str, customer_ids: Dict[str, str],
-                  api_client: GoogleAdsServiceClient,
-                  parser: parsers.BaseParser, writer_client: writer.AbsWriter,
-                  args: argparse.Namespace) -> None:
+def process_query(query: str,
+                  customer_ids: List[str],
+                  api_client: api_clients.BaseClient,
+                  reader_client: reader.AbsReader,
+                  writer_client: writer.AbsWriter,
+                  args: Dict[Any, Any] = None) -> None:
     """Reads query, extract results and stores them in a specified location.
 
     Attributes:
         query: Path to a file that contains query text.
         customer_ids: All accounts for which query will be executed.
         api_client: Client used to perform authentication to Ads API.
-        parser: Parser responsible for extracting elements from each row of
-            Ads API response.
+        reader_client: Client responsible for reading data from local storage
         writer_client: Client responsible for writing data to local/remote
             location.
-        args: Arguments that need to be passed to a query
+        args: Arguments that need to be passed to the query
     """
 
-    with open(query, "r") as f:
-        raw_query_text = f.read()
-    query_elements = AdsQueryEditor(raw_query_text).get_query_elements()
-    print(query_elements.resource_name)
-    query_text = query_elements.query_text.format(start_date=args.start_date,
-                                                  end_date=args.end_date)
-    getter = attrgetter(*query_elements.fields)
-    total_results = []
-    for customer_id in customer_ids:
-        response = api_client.search_stream(customer_id=customer_id,
-                                            query=query_text)
-        for batch in response:
-            results = [
-                api_handler.parse_ads_row(row, getter, parser,
-                                          query_elements.customizers)
-                for row in batch.results
-            ]
-            logging.info(
-                "[Query: %s] customer_id: %s, nrows - %d, size in bytes - %d",
-                query, customer_id, len(results), sys.getsizeof(results))
-            total_results.extend(results)
-
-        if query_elements.resource_name.endswith("_constant"):
-            print("Running only once")
-            break
-
-    logging.info("[Query: %s] total size in bytes - %d", query,
-                 sys.getsizeof(total_results))
-    if len(total_results) > 0:
-        output = writer_client.write(total_results, query,
-                                     query_elements.column_names)
+    query_text = reader_client.read(query)
+    query_executor = AdsQueryExecutor(api_client, query_text, query, args)
+    results = query_executor.execute(customer_ids)
+    if len(results) > 0:
+        writer_client.write(results,
+                            query_executor.query_specification.query_title,
+                            query_executor.query_specification.column_names)
     else:
         raise writer.ZeroRowException
 
@@ -92,9 +66,10 @@ def main():
     parser.add_argument("--bq.dataset", dest="dataset")
     parser.add_argument("--sql.start_date", dest="start_date")
     parser.add_argument("--sql.end_date", dest="end_date")
-    parser.add_argument("-ads-config",
+    parser.add_argument("--ads-config",
                         dest="config",
                         default=str(Path.home() / "google-ads.yaml"))
+    parser.add_argument("--api-version", dest="api_version", default=10)
     parser.add_argument("--log",
                         "--loglevel",
                         dest="loglevel",
@@ -108,46 +83,52 @@ def main():
         datefmt="%Y-%m-%d %H:%M:%S")
     logging.getLogger("google.ads.googleads.client").setLevel(logging.WARNING)
 
-    ga_service = api_clients.GoogleAdsApiClient(
-        path_to_config=args.config).get_client()
-
-    google_ads_row_parser = parsers.GoogleAdsRowParser()
+    ads_client = api_clients.GoogleAdsApiClient(path_to_config=args.config,
+                                                version=f"v{args.api_version}")
 
     writer_factory = writer.WriterFactory()
     writer_client = writer_factory.create_writer(args.save, **vars(args))
+    reader_client = reader.FileReader()
 
-    customer_ids = api_handler.get_customer_ids(ga_service, args.customer_id)
+    query_executor = AdsQueryExecutor(
+        ads_client, """
+        SELECT customer_client.id FROM customer_client
+        WHERE customer_client.manager = FALSE
+        """, None)
+    customer_ids = query_executor.execute(args.customer_id)
+
     logging.info("Total number of customer_ids is %d, accounts=[%s]",
-                 len(customer_ids), ",".join(customer_ids))
+                 len(customer_ids), ",".join(map(str, customer_ids)))
 
     with futures.ThreadPoolExecutor() as executor:
         future_to_query = {
-            executor.submit(process_query, query, customer_ids, ga_service,
-                            google_ads_row_parser, writer_client, args): query
+            executor.submit(process_query, query, customer_ids, ads_client,
+                            reader_client, writer_client, vars(args)): query
             for query in args.query
         }
         for future in futures.as_completed(future_to_query):
             query = future_to_query[future]
             try:
-                result = future.result()
+                future.result()
                 print(f"{query} executed successfully")
             except writer.ZeroRowException:
-                print(
-                    f"{query} generated ZeroRowException, please check WHERE statements."
-                )
+                print(f"""
+                    {query} generated ZeroRowException,
+                    please check WHERE statements.
+                    """)
             except GoogleAdsException as ex:
-                print(
-                    f'"{query}" failed with status '
-                    f'"{ex.error.code().name}" and includes the following errors:'
-                )
+                print(f'"{query}" failed with status '
+                      f'"{ex.error.code().name}" and includes'
+                      'the following errors:')
                 for error in ex.failure.errors:
                     print(f'\tError with message "{error.message}".')
                     if error.location:
-                        for field_path_element in error.location.field_path_elements:
+                        for field in error.location.field_path_elements:
                             print(
-                                f"\t\tOn field: {field_path_element.field_name}"
+                                f"\t\tOn field: {field.field_name}"
                             )
             except Exception as e:
+                traceback.print_tb(e.__traceback__)
                 print(f"{query} generated an exception: {str(e)}")
 
 
