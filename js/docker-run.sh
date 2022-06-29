63bc0eec7693608ccdc0aed9e040eeac7db52419
Sergei Dorogin <segy@google.com>
2022-06-29 18:32:42 +0400

    Docker support: how to run Gaarf in docker containers
    Change-Id: If624eb6d904e267c150ff31772952ff8cfcf3095

---
+++ js/docker-run.sh
@@ -0,0 +1,5 @@
+# Run a container from image "gaarf" (see docker-build.sh) with passing parameters:
+sudo docker run --mount type=bind,source="$(pwd)/.gaarfrc",target=/app/.gaarfrc \
+  --mount type=bind,source="$(pwd)/ads-scripts",target=/app/ads-scripts \
+  --mount type=bind,source="$(pwd)/service_account.json",target=/app/service_account.json \
+  gaarf ./ads-scripts/*.sql
