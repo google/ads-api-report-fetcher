91dc82058518e6c8cc8b27ca371e0e352a51c319
Andrei Markin <amarkin@google.com>
2022-07-06 01:45:58 +0400

    Add support for alternative methods for GoogleAdsApiClient
    Change-Id: I86491c0d7a36236cd2fb30ffbac9f8f7206fd89f

--- py/gaarf/api_clients.py
+++ py/gaarf/api_clients.py
@@ -11,11 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Any, Dict, Optional
 from pathlib import Path
 from google.ads.googleads.client import GoogleAdsClient  # type: ignore
 
 
 class BaseClient:
+
     def get_response(self, entity_id: str, query_text: str):
         pass
 
@@ -25,12 +27,29 @@ class GoogleAdsApiClient(BaseClient):
 
     def __init__(self,
                  path_to_config: str = default_google_ads_yaml,
+                 config_dict: Dict[str, Any] = None,
+                 yaml_str: str = None,
                  version: str = "v9"):
-        self.client = GoogleAdsClient.load_from_storage(path=path_to_config,
-                                                        version=version)
+        self.client = self._init_client(path=path_to_config,
+                                        config_dict=config_dict,
+                                        yaml_str=yaml_str,
+                                        version=version)
         self.ads_service = self.client.get_service("GoogleAdsService")
 
     def get_response(self, entity_id, query_text):
         response = self.ads_service.search_stream(customer_id=entity_id,
                                                   query=query_text)
         return response
+
+    def _init_client(self, path, config_dict, yaml_str,
+                     version) -> Optional[GoogleAdsClient]:
+        if path:
+            return GoogleAdsClient.load_from_storage(path, version)
+        if config_dict:
+            return GoogleAdsClient.load_from_dict(config_dict, version)
+        if yaml_str:
+            return GoogleAdsClient.load_from_string(yaml_str, version)
+        try:
+            return GoogleAdsClient.load_from_env(version)
+        except Exception as e:
+            raise ValueError("Cannot instantiate GoogleAdsClient")
