f3d0c7c1e4f9f4e3256a31c12d1ac1f101e663c8
Andrei Markin <amarkin@google.com>
2022-07-25 19:09:23 +0400

    Add missing LICENSE headers
    Change-Id: I35c095d6aaa5a116011b21802bf5e00847978f29

--- py/gaarf/api_clients.py
+++ py/gaarf/api_clients.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from typing import Any, Dict, Optional
 from pathlib import Path
 from google.ads.googleads.client import GoogleAdsClient  # type: ignore
