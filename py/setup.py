b16d0e06151a26d7fd2c6ce333f2724cd10ce4d6
Andrei Markin <amarkin@google.com>
2023-08-07 14:26:09 +0400

    [gaarf-py] Bump gaarf to 1.10.0
    Change-Id: Iff3296ddb7ea32c49cd3010a569f14773135e17b

--- py/setup.py
+++ py/setup.py
@@ -15,7 +15,7 @@ EXTRAS_REQUIRE['full'] = list(set(chain(*EXTRAS_REQUIRE.values())))
 
 setup(
     name="google-ads-api-report-fetcher",
-    version="1.9.0",
+    version="1.10.0",
     python_requires=">3.9",
     description=
     "Library for fetching reports from Google Ads API and saving them locally / BigQuery.",
