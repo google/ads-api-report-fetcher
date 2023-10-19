24916b5ca95739e0bd2825d638172b817cfc371f
Sergei Dorogin <segy@google.com>
2023-10-19 15:55:02 +0400

    gaarf-wf: two new arguments for workflow and gaarf-getcids CF: `customer_ids_offset` and `customer_ids_batchsize`. They allow to limit a final list of accounts to process by returning a sublist at given offset and of given size of the expanded and filtered list of accounts.
    Change-Id: Iff2444dcabe29a696030ab23e0ede3a50cb42f5e

--- gcp/functions/build/src/gaarf-getcids.js
+++ gcp/functions/build/src/gaarf-getcids.js
@@ -31,8 +31,8 @@ async function main_getcids_unsafe(req, res, logger) {
     if (!adsConfig.login_customer_id && customerIds && customerIds.length === 1) {
         adsConfig.login_customer_id = customerIds[0];
     }
-    const ads_client = new google_ads_api_report_fetcher_1.GoogleAdsApiClient(adsConfig);
-    customerIds = await ads_client.getCustomerIds(customerIds);
+    const adsClient = new google_ads_api_report_fetcher_1.GoogleAdsApiClient(adsConfig);
+    customerIds = await adsClient.getCustomerIds(customerIds);
     let customer_ids_query = '';
     if (req.body && req.body.customer_ids_query) {
         customer_ids_query = req.body.customer_ids_query;
@@ -42,8 +42,36 @@ async function main_getcids_unsafe(req, res, logger) {
     }
     if (customer_ids_query) {
         await logger.info(`Fetching customer id using custom query: ${customer_ids_query}`);
-        const executor = new google_ads_api_report_fetcher_1.AdsQueryExecutor(ads_client);
+        const executor = new google_ads_api_report_fetcher_1.AdsQueryExecutor(adsClient);
         customerIds = await executor.getCustomerIds(customerIds, customer_ids_query);
+    }
+    if (customerIds.length) {
+        customerIds.sort();
+        // extract a subset of CIDs if offset/batch are specified
+        let offset = 0;
+        if (req.query.customer_ids_offset) {
+            offset = parseInt(req.query.customer_ids_offset);
+            if (isNaN(offset)) {
+                throw new Error('customer_ids_offset should be a number');
+            }
+        }
+        let batchsize = 0;
+        if (req.query.customer_ids_batchsize) {
+            batchsize = parseInt(req.query.customer_ids_batchsize);
+            if (isNaN(offset)) {
+                throw new Error('customer_ids_batchsize should be a number');
+            }
+        }
+        const cids_length = customerIds.length;
+        if (batchsize > 0) {
+            customerIds = customerIds.slice(offset, offset + batchsize);
+        }
+        else if (offset > 0) {
+            customerIds = customerIds.slice(offset);
+        }
+        if (cids_length !== customerIds.length) {
+            await logger.info(`Reshaped cids array from ${cids_length} to ${customerIds.length} items`);
+        }
     }
     res.json(customerIds);
     res.end();
@@ -56,7 +84,6 @@ const main_getcids = async (req, res) => {
         await main_getcids_unsafe(req, res, logger);
     }
     catch (e) {
-        console.log(e);
         await logger.error(e.message, { error: e });
         res.status(500).send(e.message).end();
     }
--- gcp/functions/src/gaarf-getcids.ts
+++ gcp/functions/src/gaarf-getcids.ts
@@ -45,8 +45,8 @@ async function main_getcids_unsafe(
   if (!adsConfig.login_customer_id && customerIds && customerIds.length === 1) {
     adsConfig.login_customer_id = customerIds[0];
   }
-  const ads_client = new GoogleAdsApiClient(adsConfig);
-  customerIds = await ads_client.getCustomerIds(customerIds);
+  const adsClient = new GoogleAdsApiClient(adsConfig);
+  customerIds = await adsClient.getCustomerIds(customerIds);
   let customer_ids_query = '';
   if (req.body && req.body.customer_ids_query) {
     customer_ids_query = <string>req.body.customer_ids_query;
@@ -59,11 +59,41 @@ async function main_getcids_unsafe(
     await logger.info(
       `Fetching customer id using custom query: ${customer_ids_query}`
     );
-    const executor = new AdsQueryExecutor(ads_client);
+    const executor = new AdsQueryExecutor(adsClient);
     customerIds = await executor.getCustomerIds(
       customerIds,
       customer_ids_query
     );
+  }
+  if (customerIds.length) {
+    customerIds.sort();
+
+    // extract a subset of CIDs if offset/batch are specified
+    let offset = 0;
+    if (req.query.customer_ids_offset) {
+      offset = parseInt(<string>req.query.customer_ids_offset);
+      if (isNaN(offset)) {
+        throw new Error('customer_ids_offset should be a number');
+      }
+    }
+    let batchsize = 0;
+    if (req.query.customer_ids_batchsize) {
+      batchsize = parseInt(<string>req.query.customer_ids_batchsize);
+      if (isNaN(offset)) {
+        throw new Error('customer_ids_batchsize should be a number');
+      }
+    }
+    const cids_length = customerIds.length;
+    if (batchsize > 0) {
+      customerIds = customerIds.slice(offset, offset + batchsize);
+    } else if (offset > 0) {
+      customerIds = customerIds.slice(offset);
+    }
+    if (cids_length !== customerIds.length) {
+      await logger.info(
+        `Reshaped cids array from ${cids_length} to ${customerIds.length} items`
+      );
+    }
   }
 
   res.json(customerIds);
@@ -85,7 +115,6 @@ export const main_getcids: HttpFunction = async (
   try {
     await main_getcids_unsafe(req, res, logger);
   } catch (e) {
-    console.log(e);
     await logger.error(e.message, {error: e});
     res.status(500).send(e.message).end();
   }
