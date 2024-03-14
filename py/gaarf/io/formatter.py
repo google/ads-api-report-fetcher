faa56d67e0741e83c841b1da15c582d0942cac87
Andrei Markin <amarkin@google.com>
2024-03-14 16:07:49 +0400

    Update io.formatter
    * Add documentation
    * Add missing from __future__ import annotations to support python3.8+
    
    Change-Id: Ic6d97a0b59f6b11a86b40ebd29827bba5db5d616

--- py/gaarf/io/formatter.py
+++ py/gaarf/io/formatter.py
@@ -11,39 +11,57 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import annotations
 
-from typing import Type
-import abc
 from enum import Enum
 from pathlib import Path
-import proto  # type: ignore
 
+import proto  # type: ignore
 from gaarf.report import GaarfReport
 
 
 class FormattingStrategy:
+    """Interface for all formatting strategies applied to GaarfReport."""
 
     def apply_transformations(self, report: GaarfReport) -> GaarfReport:
+        """Applies class transformation to report."""
         raise NotImplementedError
 
-    def _cast_to_enum(self, enum: Type[Enum], value: str | Enum) -> Enum:
+    def _cast_to_enum(self, enum: type[Enum], value: str | Enum) -> Enum:
+        """Ensures that strings are always converted to Enums."""
         return enum[value.upper()] if isinstance(value, str) else value
 
 
 class ArrayHandling(Enum):
+    """Specifies acceptable options for ArrayHandlingStrategy."""
     STRINGS = 1
     ARRAYS = 2
 
 
 class ArrayHandlingStrategy(FormattingStrategy):
+    """Handles arrays in the report.
+
+    Arrays can be left as-is or converted to strings with required delimiter.
+
+    Attributes:
+        type_: Type of array handling (ARRAYS, STRINGS).
+        delimiter: Symbol used as delimiter when type_ is STRINGS.
+    """
 
     def __init__(self,
                  type_: ArrayHandling | str = ArrayHandling.STRINGS,
-                 delimiter: str = "|") -> None:
+                 delimiter: str = '|') -> None:
+        """Initializes strategy based on type_ and delimiter.
+
+        Args:
+            type_: Type of array handling (ARRAYS, STRINGS).
+            delimiter: Symbol used as delimiter when type_ is STRINGS.
+        """
         self.type_ = self._cast_to_enum(ArrayHandling, type_)
         self.delimiter = delimiter
 
     def apply_transformations(self, report: GaarfReport) -> GaarfReport:
+        """Replaces arrays in the report."""
         if self.type_ == ArrayHandling.ARRAYS:
             return report
         formatted_rows = []
@@ -52,32 +70,49 @@ class ArrayHandlingStrategy(FormattingStrategy):
             for field in row:
                 if isinstance(
                         field,
-                    (list,
-                     proto.marshal.collections.repeated.RepeatedComposite,
+                    (list, proto.marshal.collections.repeated.RepeatedComposite,
                      proto.marshal.collections.repeated.Repeated)):
                     formatted_row.append(
-                        self.delimiter.join(
-                            [str(element) for element in field]))
+                        self.delimiter.join([str(element) for element in field
+                                            ]))
                 else:
                     formatted_row.append(field)
             formatted_rows.append(formatted_row)
-        return GaarfReport(results=formatted_rows,
-                           column_names=report.column_names)
+        return GaarfReport(
+            results=formatted_rows, column_names=report.column_names)
 
 
 def format_report_for_writing(
         report: GaarfReport,
         formatting_strategies: list[FormattingStrategy]) -> GaarfReport:
+    """Applies formatting strategies to report.
+
+    Args:
+        report: Report that needs to be formatted.
+        formatting_strategies: Strategies to be applied to report.
+
+    Returns:
+        New report with updated data.
+    """
     for strategy in formatting_strategies:
         report = strategy.apply_transformations(report)
     return report
 
 
 def format_extension(path_object: str,
-                     current_extension: str = ".sql",
-                     new_extension: str = "") -> str:
+                     current_extension: str = '.sql',
+                     new_extension: str = '') -> str:
+    """Formats query path to required extension.
+
+    Args:
+        path_object: Path to query.
+        current_extension: Extension of the query.
+        new_extension: Required extension
+
+    Returns:
+       Path with an updated extension.
+    """
     path_object_name = Path(path_object).name
-    if len(path_object_name.split(".")) > 1:
+    if len(path_object_name.split('.')) > 1:
         return path_object_name.replace(current_extension, new_extension)
-    else:
-        return f"{path_object}{new_extension}"
+    return f'{path_object}{new_extension}'
