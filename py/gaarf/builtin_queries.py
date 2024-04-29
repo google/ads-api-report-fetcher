6d36b20c11c549de691a74a45759eab1c3ad78e5
Andrei Markin <amarkin@google.com>
2024-04-29 13:47:12 +0400

    [gaarf-py] Update ocid_mapping builtin query
    * Add tests for `get_ocid_mapping` function
    * Add missing documentation and typing
    * Format code
    
    Change-Id: I1e8a18d32e465961548f3bf619be2ad1d52c203a

--- py/gaarf/builtin_queries.py
+++ py/gaarf/builtin_queries.py
@@ -11,36 +11,65 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.import proto
+"""Defines queries that are accessible via `builtin` namespace.
 
-from typing import Callable, Dict, List
-from collections import abc, defaultdict
+Built-in queries provides quick access to important elements in Google Ads API
+with a simple SQL syntax.
+All built-in queries are available in `builtin` namespace
+(i.e. `builtin.ocid_mapping`) and can be added to SQL scripts
+(i.e. `SELECT * FROM builtin.ocid_mapping`).
+
+"""
+from __future__ import annotations
+
 import functools
 import operator
-import pandas as pd
 import re
+from collections import abc
+from collections import defaultdict
+from typing import Callable
 
-from .report import GaarfRow, GaarfReport
+import gaarf
+from gaarf import query_executor
 
 
-def get_ocid_mapping(report_fetcher, accounts) -> GaarfReport:
-    query = (
-        "SELECT customer.id AS account_id, metrics.optimization_score_url  AS url FROM customer"
-    )
+def get_ocid_mapping(report_fetcher: query_executor.AdsReportFetcher,
+                     accounts: list[str]) -> gaarf.report.GaarfReport:
+    """Returns mapping between external customer_id and OCID parameter.
+
+
+    OCID parameter is used to build links to Google Ads entities in UI.
+
+    Args:
+        report_fetcher: An instance of AdsReportFetcher.
+        accounts: Google Ads accounts to get data on OCID mapping.
+
+    Returns:
+        Report with mapping between external customer_id and OCID parameter.
+    """
+    query = ('SELECT customer.id AS account_id, '
+             'metrics.optimization_score_url  AS url FROM customer')
     mapping = []
     for account in accounts:
         if (report := report_fetcher.fetch(query, account)):
             for row in report:
-                if (ocid := re.findall("ocid=(\w+)", row.url)):
-                    mapping.append((row.account_id, ocid[0]))
+                if (ocid := re.findall(r'ocid=(\w+)', row.url)):
+                    mapping.append([row.account_id, ocid[0]])
                     break
             if not ocid:
-                mapping.append((int(account), "0"))
+                mapping.append([int(account), '0'])
         else:
-            mapping.append((int(account), "0"))
-    return GaarfReport(results=mapping, column_names=("account_id", "ocid"))
+            mapping.append([int(account), '0'])
+    return gaarf.report.GaarfReport(
+        results=mapping, column_names=[
+            'account_id',
+            'ocid',
+        ])
 
 
-def get_account_hierarchy_flattened(report_fetcher, accounts) -> GaarfReport:
+def get_account_hierarchy_flattened(
+        report_fetcher: query_executor.AdsReportFetcher,
+        accounts: list[str]) -> gaarf.report.GaarfReport:
     SEED_ACCOUNTS_QUERY = """
     SELECT
         customer_client.level AS level,
@@ -59,85 +88,84 @@ def get_account_hierarchy_flattened(report_fetcher, accounts) -> GaarfReport:
     FROM customer_client_link
     """
 
-    seed_accounts = accounts if isinstance(
-        accounts, abc.MutableSequence) else [accounts]
+    seed_accounts = accounts if isinstance(accounts,
+                                           abc.MutableSequence) else [accounts]
     hierarchy = report_fetcher.fetch(SEED_ACCOUNTS_QUERY, seed_accounts)
     level_mapping = defaultdict(list)
     for row in hierarchy:
         if row.is_manager:
             level_mapping[row.level].append(row.account_id)
 
-    reports: List[GaarfReport] = []
+    reports: list[gaarf.report.GaarfReport] = []
     for level, accounts in level_mapping.items():
         if accounts:
             report_fetcher.customer_ids = accounts
             reports.append(
-                report_fetcher.fetch(
-                    NESTED_ACCOUNTS_QUERY.format(level=level)))
+                report_fetcher.fetch(NESTED_ACCOUNTS_QUERY.format(level=level)))
 
     combined_report = functools.reduce(operator.add, reports)
     df = combined_report.to_pandas()
     max_level = max(df.level)
-    root_mcc_df = df[df.level == 0][["mcc_id", "mcc_name", "account_id"]]
+    root_mcc_df = df[df.level == 0][['mcc_id', 'mcc_name', 'account_id']]
     root_mcc_df = root_mcc_df.rename(columns={
-        "mcc_id": "root_mcc_id",
-        "mcc_name": "root_mcc_name"
+        'mcc_id': 'root_mcc_id',
+        'mcc_name': 'root_mcc_name'
     })
-    root_mcc_df["account_id"] = root_mcc_df["account_id"].astype(int)
-    selectable_columns = ["root_mcc_id", "root_mcc_name"]
+    root_mcc_df['account_id'] = root_mcc_df['account_id'].astype(int)
+    selectable_columns = ['root_mcc_id', 'root_mcc_name']
     if max_level == 0:
-        return GaarfReport.from_pandas(root_mcc_df)
+        return gaarf.report.GaarfReport.from_pandas(root_mcc_df)
     else:
         for i in range(1, max(df.level) + 1):
             if i == 1:
                 n_mcc_df = df[df.level == i][[
-                    "mcc_id", "mcc_name", "account_id"
+                    'mcc_id', 'mcc_name', 'account_id'
                 ]]
-                mcc_ids = n_mcc_df.merge(root_mcc_df,
-                                         how="right",
-                                         left_on="mcc_id",
-                                         right_on="account_id")[[
-                                             "root_mcc_id", "root_mcc_name",
-                                             "account_id_x", "mcc_id",
-                                             "mcc_name", "account_id_y"
-                                         ]]
+                mcc_ids = n_mcc_df.merge(
+                    root_mcc_df,
+                    how='right',
+                    left_on='mcc_id',
+                    right_on='account_id')[[
+                        'root_mcc_id', 'root_mcc_name', 'account_id_x',
+                        'mcc_id', 'mcc_name', 'account_id_y'
+                    ]]
             else:
                 previous_level = i - 1
                 n_minus_1_mcc_df = df[df.level == previous_level][[
-                    "mcc_id", "mcc_name", "account_id"
+                    'mcc_id', 'mcc_name', 'account_id'
                 ]]
                 n_minus_1_mcc_df = n_minus_1_mcc_df.rename(
                     columns={
-                        "mcc_id": f"level_{previous_level}_mcc_id",
-                        "mcc_name": f"level_{previous_level}_mcc_name",
+                        'mcc_id': f'level_{previous_level}_mcc_id',
+                        'mcc_name': f'level_{previous_level}_mcc_name',
                     })
                 mcc_ids = n_mcc_df.merge(
                     n_minus_1_mcc_df,
-                    how="right",
-                    left_on="mcc_id",
-                    right_on="account_id")[[
-                        f"level_{previous_level}_mcc_id",
-                        f"level_{previous_level}_mcc_name", "account_id_x",
-                        "mcc_id", "mcc_name", "account_id_y"
+                    how='right',
+                    left_on='mcc_id',
+                    right_on='account_id')[[
+                        f'level_{previous_level}_mcc_id',
+                        f'level_{previous_level}_mcc_name', 'account_id_x',
+                        'mcc_id', 'mcc_name', 'account_id_y'
                     ]]
 
-            mcc_ids.mcc_name.fillna("Direct", inplace=True)
+            mcc_ids.mcc_name.fillna('Direct', inplace=True)
             mcc_ids.account_id_x.fillna(mcc_ids.account_id_y, inplace=True)
             mcc_ids.mcc_id.fillna(mcc_ids.account_id_y, inplace=True)
             mcc_ids = mcc_ids.rename(
                 columns={
-                    "mcc_name": f"level_{i}_mcc_name",
-                    "account_id_x": "account_id",
-                    "mcc_id": f"level_{i}_mcc_id"
+                    'mcc_name': f'level_{i}_mcc_name',
+                    'account_id_x': 'account_id',
+                    'mcc_id': f'level_{i}_mcc_id'
                 })[selectable_columns +
-                   [f"level_{i}_mcc_id", f"level_{i}_mcc_name", "account_id"]]
-            mcc_ids[f"level_{i}_mcc_id"] = mcc_ids[f"level_{i}_mcc_id"].astype(
+                   [f'level_{i}_mcc_id', f'level_{i}_mcc_name', 'account_id']]
+            mcc_ids[f'level_{i}_mcc_id'] = mcc_ids[f'level_{i}_mcc_id'].astype(
                 int)
-        mcc_ids["account_id"] = mcc_ids["account_id"].astype(int)
-        return GaarfReport.from_pandas(mcc_ids)
+        mcc_ids['account_id'] = mcc_ids['account_id'].astype(int)
+        return gaarf.report.GaarfReport.from_pandas(mcc_ids)
 
 
-BUILTIN_QUERIES: Dict[str, Callable] = {
-    "ocid_mapping": get_ocid_mapping,
-    "account_hierarchy_flattened": get_account_hierarchy_flattened
+BUILTIN_QUERIES: dict[str, Callable] = {
+    'ocid_mapping': get_ocid_mapping,
+    'account_hierarchy_flattened': get_account_hierarchy_flattened
 }
---
+++ py/tests/unit/test_builtin_queries.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+import pytest
+from gaarf import api_clients
+from gaarf import builtin_queries
+from gaarf import query_executor
+from gaarf import report
+
+
+class TestBuiltInQueries:
+
+    @pytest.fixture
+    def test_client(self, mocker):
+        mocker.patch('google.ads.googleads.client.oauth2', return_value=[])
+        return api_clients.GoogleAdsApiClient()
+
+    @pytest.fixture
+    def fake_report_fetcher(self, mocker, test_client):
+        data = [
+            [
+                1,
+                'https://ads.google.com/aw/accounts?__u=1&&ocid=12345',
+            ],
+        ]
+        column_names = [
+            'account_id',
+            'url',
+        ]
+        mocker.patch(
+            'gaarf.query_executor.AdsReportFetcher.fetch',
+            return_value=report.GaarfReport(data, column_names))
+        return query_executor.AdsReportFetcher(test_client)
+
+    @pytest.fixture
+    def missing_ocid_fake_report_fetcher(self, mocker, test_client):
+        data = [
+            [
+                1,
+                '',
+            ],
+        ]
+        column_names = [
+            'account_id',
+            'url',
+        ]
+        mocker.patch(
+            'gaarf.query_executor.AdsReportFetcher.fetch',
+            return_value=report.GaarfReport(data, column_names))
+        return query_executor.AdsReportFetcher(test_client)
+
+    @pytest.fixture
+    def empty_fake_report_fetcher(self, mocker, test_client):
+        column_names = [
+            'account_id',
+            'url',
+        ]
+        mocker.patch(
+            'gaarf.query_executor.AdsReportFetcher.fetch',
+            return_value=report.GaarfReport([], column_names))
+        return query_executor.AdsReportFetcher(test_client)
+
+    def test_get_ocid_mapping_returns_correct_result(self, fake_report_fetcher):
+        account_id = 1
+        expected_report = report.GaarfReport(
+            results=[
+                [
+                    account_id,
+                    '12345',
+                ],
+            ],
+            column_names=[
+                'account_id',
+                'ocid',
+            ])
+        ocid_mapping = builtin_queries.get_ocid_mapping(
+            fake_report_fetcher, accounts=[
+                account_id,
+            ])
+        assert ocid_mapping == expected_report
+
+    def test_get_ocid_mapping_returns_placeholder_for_missing_ocid_report(
+            self, missing_ocid_fake_report_fetcher):
+        account_id = 1
+        expected_report = report.GaarfReport(
+            results=[
+                [
+                    account_id,
+                    '0',
+                ],
+            ], column_names=[
+                'account_id',
+                'ocid',
+            ])
+        ocid_mapping = builtin_queries.get_ocid_mapping(
+            missing_ocid_fake_report_fetcher, accounts=[
+                account_id,
+            ])
+        assert ocid_mapping == expected_report
+
+    def test_get_ocid_mapping_returns_placeholder_for_empty_report(
+            self, empty_fake_report_fetcher):
+        account_id = 1
+        expected_report = report.GaarfReport(
+            results=[
+                [
+                    account_id,
+                    '0',
+                ],
+            ], column_names=[
+                'account_id',
+                'ocid',
+            ])
+        ocid_mapping = builtin_queries.get_ocid_mapping(
+            empty_fake_report_fetcher, accounts=[
+                account_id,
+            ])
+        assert ocid_mapping == expected_report
