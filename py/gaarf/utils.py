dfa85db956a8da85b6f6b431b8590aadbe795ec8
Andrei Markin <amarkin@google.com>
2024-05-06 16:02:41 +0400

    [gaarf-py] Remove deprecated definition of get_customer_ids
    Call `report_fetcher.expand_mcc` instead of implementing from scratch.
    
    Change-Id: I8adbc56a56f48a9b91a9ef5d8aba4a2f1979ada5

--- py/gaarf/utils.py
+++ py/gaarf/utils.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,23 +10,20 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.import proto
+# limitations under the License.
 """Module for various utility functions."""
 from __future__ import annotations
 
 import warnings
 from collections.abc import MutableSequence
-from collections.abc import Sequence
 
 from gaarf import api_clients
-from gaarf import query_editor
 from gaarf import query_executor
-from gaarf import report
 
 
 def get_customer_ids(ads_client: api_clients.GoogleAdsApiClient,
                      customer_id: str | MutableSequence,
-                     customer_ids_query: str | None = None) -> Sequence[str]:
+                     customer_ids_query: str | None = None) -> list[str]:
     """Gets list of customer_ids from an MCC account.
 
     Args:
@@ -38,32 +35,11 @@ def get_customer_ids(ads_client: api_clients.GoogleAdsApiClient,
         All customer_ids from MCC satisfying the condition.
     """
 
-    query = """
-    SELECT customer_client.id FROM customer_client
-    WHERE customer_client.manager = FALSE AND customer_client.status = ENABLED
-    """
     warnings.warn(
         '`get_customer_ids` will be deprecated, '
         'use `AdsReportFetcher.expand_mcc` or `AdsQueryExecutor.expand_mcc` '
         'methods instead',
         category=DeprecationWarning,
         stacklevel=3)
-    query_specification = query_editor.QuerySpecification(query).generate()
-    if not isinstance(customer_id, MutableSequence):
-        customer_id = customer_id.split(',')
     report_fetcher = query_executor.AdsReportFetcher(ads_client)
-    customer_ids = report_fetcher.fetch(query_specification,
-                                        customer_id).to_list()
-    if customer_ids_query:
-        query_specification = query_editor.QuerySpecification(
-            customer_ids_query).generate()
-        customer_ids = report_fetcher.fetch(query_specification, customer_ids)
-        customer_ids = [
-            row[0] if isinstance(row, report.GaarfRow) else row
-            for row in customer_ids
-        ]
-
-    customer_ids = list(
-        set([customer_id for customer_id in customer_ids if customer_id != 0]))
-
-    return customer_ids
+    return report_fetcher.expand_mcc(customer_id, customer_ids_query)
---
+++ py/tests/unit/test_utils.py
@@ -0,0 +1,79 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+from __future__ import annotations
+
+import dataclasses
+
+import pytest
+from gaarf import api_clients
+from gaarf import parsers
+from gaarf import query_executor
+from gaarf import utils
+
+
+@dataclasses.dataclass
+class FakeResponse:
+    data: list[list[parsers.GoogleAdsRowElement]]
+
+    def __iter__(self):
+        for result in self.data:
+            yield FakeBatch(result)
+
+
+@dataclasses.dataclass
+class FakeBatch:
+    results: list[list]
+
+
+@dataclasses.dataclass
+class CustomerClient:
+    id: int
+
+
+@dataclasses.dataclass
+class FakeGoogleAdsRowElement:
+    customer_client: CustomerClient
+
+
+@pytest.fixture
+def test_client(mocker):
+    fake_results = [
+        [
+            FakeGoogleAdsRowElement(CustomerClient(1)),
+        ],
+    ]
+    fake_response = FakeResponse(data=fake_results)
+    mocker.patch('google.ads.googleads.client.oauth2', return_value=[])
+    mocker.patch(
+        'gaarf.api_clients.GoogleAdsApiClient.get_response',
+        return_value=fake_response)
+    return api_clients.GoogleAdsApiClient()
+
+
+@pytest.fixture
+def fake_report_fetcher(mocker, test_client):
+    data = ['1']
+    mocker.patch(
+        'gaarf.query_executor.AdsReportFetcher.expand_mcc', return_value=data)
+    return query_executor.AdsReportFetcher(test_client)
+
+
+def test_calling_get_customer_ids_is_deprecated(test_client):
+    with pytest.warns(DeprecationWarning) as w:
+        utils.get_customer_ids(test_client, '1')
+        assert len(w) == 1
+        assert str(w[0].message) == ('`get_customer_ids` will be deprecated, '
+                                     'use `AdsReportFetcher.expand_mcc` or '
+                                     '`AdsQueryExecutor.expand_mcc` '
+                                     'methods instead')
