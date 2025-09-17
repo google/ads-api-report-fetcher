5e0e75a5e712c5d8ca9fd39592573abf3ee704ad
Andrei Markin <amarkin@google.com>
2025-09-17 11:43:11 +0400

    [gaarf-py] tests: update end-to-end tests
    * Implement skipped tests
    * Switch to `shell=True` for more readable command specification
    * Check for return codes to avoid flaky tests
    
    Change-Id: I3f12017777a2523df919e1aafede36e70efaaf85

--- py/tests/end-to-end/test_gaarf_cli_util.py
+++ py/tests/end-to-end/test_gaarf_cli_util.py
@@ -10,6 +10,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# pylint: disable=C0330, g-bad-import-order, g-multiple-import
+
 from __future__ import annotations
 
 import glob
@@ -26,117 +29,122 @@ _TEST_ADS_ACCOUNT = os.getenv('TEST_ADS_ACCOUNT')
 @pytest.mark.e2e
 def test_gaarf_version():
   result = subprocess.run(
-    ['gaarf', '--version'], capture_output=True, text=True, check=False
+    'gaarf --version', capture_output=True, text=True, shell=True, check=False
   )
-  assert not result.stderr
-  assert 'gaarf version' in result.stdout
+  assert result.returncode == 0
+  assert 'gaarf version' in str(result.stdout)
 
 
-@pytest.mark.skip('Failing for FieldError')
 @pytest.mark.e2e
 def test_gaarf_console_input():
+  query = 'SELECT customer.id FROM customer LIMIT 1'
+  command = f'gaarf "{query}" --input console'
   result = subprocess.run(
-    [
-      'gaarf',
-      '"SELECT customer.id FROM customer LIMIT 1"',
-      '--input=console',
-    ],
+    command,
     capture_output=True,
-    text=True,
+    shell=True,
     check=False,
   )
-  assert not result.stderr
+  assert result.returncode == 0
+  assert f'{query} executed successfully' in str(result.stdout)
 
 
 @pytest.mark.e2e
 def test_gaarf_many_queries():
   queries = glob.glob(f'{_QUERIES_PATH}/*.sql')
+  command = (
+    f'gaarf {' '.join(queries)} '
+    '--output=console '
+    f'--account={_TEST_ADS_ACCOUNT} '
+    '--disable-account-expansion'
+  )
   result = subprocess.run(
-    ['gaarf']
-    + queries
-    + [
-      '--output=console',
-      f'--account={_TEST_ADS_ACCOUNT}',
-      '--disable-account-expansion',
-    ],
+    command,
     capture_output=True,
     text=True,
+    shell=True,
     check=False,
   )
-  out = result.stdout
-  assert not result.stderr
+  assert result.returncode == 0
+  out = str(result.stdout)
   assert 'Running queries in parallel' in out
+  assert 'Skipping account expansion' in out
   for query in queries:
     assert f'{query} executed successfully' in out
 
 
 @pytest.mark.e2e
 def test_gaarf_no_queries():
+  command = 'gaarf --output console'
   result = subprocess.run(
-    [
-      'gaarf',
-      '--output=console',
-    ],
+    command,
     capture_output=True,
     text=True,
+    shell=True,
     check=False,
   )
+  assert result.returncode == 1
   assert 'GaarfMissingQueryException' in result.stderr
   assert 'Please provide one or more queries to run' in result.stderr
 
 
 @pytest.mark.e2e
 def test_gaarf_raises_error_on_invalid_google_ads_config():
+  command = (
+    'gaarf "SELECT customer.id FROM customer LIMIT 1" '
+    '--ads-config=missing-google-ads.yaml'
+  )
   result = subprocess.run(
-    [
-      'gaarf',
-      '"SELECT customer.id FROM customer LIMIT 1"',
-      f'--ads-config={_QUERIES_PATH}/test-google-ads.yaml',
-    ],
+    command,
     capture_output=True,
     text=True,
+    shell=True,
     check=False,
   )
 
-  assert result.stderr
+  assert result.returncode == 1
 
 
 @pytest.mark.e2e
 def test_gaarf_runs_queries_sequentially():
   queries = glob.glob(f'{_QUERIES_PATH}/*.sql')
+  command = (
+    f'gaarf {' '.join(queries)} '
+    '--output=console '
+    f'--account={_TEST_ADS_ACCOUNT} '
+    '--no-parallel-queries'
+  )
   result = subprocess.run(
-    ['gaarf']
-    + queries
-    + [
-      '--output=console',
-      f'--account={_TEST_ADS_ACCOUNT}',
-      '--no-parallel-queries',
-    ],
+    command,
     capture_output=True,
     text=True,
+    shell=True,
     check=False,
   )
+  assert result.returncode == 0
+  out = str(result.stdout)
   assert 'Running queries sequentially' in result.stdout
+  for query in queries:
+    assert f'{query} executed successfully' in out
 
 
 @pytest.mark.e2e
 def test_gaarf_only_saves_config_during_dry_run(tmp_path):
   tmp_config_path = tmp_path / 'config.yaml'
   queries = glob.glob(f'{_QUERIES_PATH}/*.sql')
+  command = (
+    f'gaarf {' '.join(queries)} --output=console --dry-run',
+    '--save-config',
+    f'--config-destination={tmp_config_path}',
+  )
   result = subprocess.run(
-    ['gaarf']
-    + queries
-    + [
-      '--output=console',
-      '--dry-run',
-      '--save-config',
-      f'--config-destination={tmp_config_path}',
-    ],
+    command,
     capture_output=True,
     text=True,
+    shell=True,
     check=False,
   )
-  out = result.stdout
-  assert not result.stderr
+  out = str(result.stdout)
+  assert result.returncode == 0
   assert 'initialized config' not in out
   assert 'Running queries' not in out
