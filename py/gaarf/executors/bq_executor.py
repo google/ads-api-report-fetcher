71b4ebcf405efeb30747f715fbe7aeba1d99a5cf
Andrei Markin <amarkin@google.com>
2025-10-20 15:05:11 +0400

    [gaarf-py] chore: migrate to garf-executor packages for BigQuery and SqlAlchemy executors
    Change-Id: I44a8ca19effa2926dc5a11cf9945f17ac0823adc

--- py/gaarf/executors/bq_executor.py
+++ py/gaarf/executors/bq_executor.py
@@ -11,55 +11,23 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# pylint: disable=C0330, g-bad-import-order, g-multiple-import
+
 """Module for executing queries in BigQuery."""
 
 from __future__ import annotations
 
-try:
-  from google.cloud import bigquery  # type: ignore
-except ImportError as e:
-  raise ImportError(
-    'Please install google-ads-api-report-fetcher with BigQuery support '
-    '- `pip install google-ads-api-report-fetcher[bq]`'
-  ) from e
-
 import logging
 
 import pandas as pd
-from google.cloud import exceptions as google_cloud_exceptions
-
-from gaarf import query_post_processor
+from garf_executors import bq_executor, execution_context
 
 logger = logging.getLogger(__name__)
 
 
-class BigQueryExecutorException(Exception):
-  """Error when executor fails to run query."""
-
-
-class BigQueryExecutor(query_post_processor.PostProcessorMixin):
-  """Handles query execution in BigQuery.
-
-  Attributes:
-      project_id: Google Cloud project id.
-      location: BigQuery dataset location.
-      client: BigQuery client.
-  """
-
-  def __init__(self, project_id: str, location: str | None = None) -> None:
-    """Initializes BigQueryExecutor.
-
-    Args:
-        project_id: Google Cloud project id.
-        location: BigQuery dataset location.
-    """
-    self.project_id = project_id
-    self.location = location
-
-  @property
-  def client(self) -> bigquery.Client:
-    """Instantiates bigquery client."""
-    return bigquery.Client(self.project_id)
+class BigQueryExecutor(bq_executor.BigQueryExecutor):
+  """Handles query execution in BigQuery."""
 
   def execute(
     self, script_name: str, query_text: str, params: dict | None = None
@@ -74,50 +42,15 @@ class BigQueryExecutor(query_post_processor.PostProcessorMixin):
     Returns:
         DataFrame if query returns some data otherwise empty DataFrame.
     """
-    query_text = self.replace_params_template(query_text, params)
-    job = self.client.query(query_text)
-    try:
-      result = job.result()
-      logger.debug('%s launched successfully', script_name)
-      if result.total_rows:
-        return result.to_dataframe()
-      return pd.DataFrame()
-    except google_cloud_exceptions.GoogleCloudError as e:
-      raise BigQueryExecutorException(e) from e
-
-  def create_datasets(self, macros: dict | None) -> None:
-    """Creates datasets in BQ based on values in a dict.
-
-    If dict contains keys with 'dataset' in them, then values for such keys
-    are treated as dataset names.
-
-    Args:
-        macros: Mapping containing data for query execution.
-    """
-    if macros and (datasets := extract_datasets(macros)):
-      for dataset in datasets:
-        dataset_id = f'{self.project_id}.{dataset}'
-        try:
-          self.client.get_dataset(dataset_id)
-        except google_cloud_exceptions.NotFound:
-          bq_dataset = bigquery.Dataset(dataset_id)
-          bq_dataset.location = self.location
-          self.client.create_dataset(bq_dataset, timeout=30)
-          logger.debug('Created new dataset %s', dataset_id)
+    context = (
+      execution_context.ExecutionContext(query_parameters=params)
+      if params
+      else execution_context.ExecutionContext()
+    )
+    report = super().execute(
+      query=query_text, title=script_name, context=context
+    )
+    return report.to_pandas()
 
 
-def extract_datasets(macros: dict | None) -> list[str]:
-  """Finds dataset-related keys based on values in a dict.
-
-  If dict contains keys with 'dataset' in them, then values for such keys
-  are treated as dataset names.
-
-  Args:
-      macros: Mapping containing data for query execution.
-
-  Returns:
-      Possible names of datasets.
-  """
-  if not macros:
-    return []
-  return [value for macro, value in macros.items() if 'dataset' in macro]
+extract_datasets = bq_executor.extract_datasets
--- py/gaarf/executors/sql_executor.py
+++ py/gaarf/executors/sql_executor.py
@@ -11,41 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# pylint: disable=C0330, g-bad-import-order, g-multiple-import
+
 """Defines mechanism for executing queries via SqlAlchemy."""
 
 from __future__ import annotations
 
-try:
-  import sqlalchemy
-except ImportError as e:
-  raise ImportError(
-    'Please install google-ads-api-report-fetcher with sqlalchemy support '
-    '- `pip install google-ads-api-report-fetcher[sqlalchemy]`'
-  ) from e
-
-import logging
-import re
 from typing import Any
 
 import pandas as pd
+from garf_executors import execution_context, sql_executor
 
-from gaarf import query_post_processor
 
-
-class SqlAlchemyQueryExecutor(query_post_processor.PostProcessorMixin):
-  """Handles query execution via SqlAlchemy.
-
-  Attributes:
-      engine: Initialized Engine object to operated on a given database.
-  """
-
-  def __init__(self, engine: sqlalchemy.engine.base.Engine) -> None:
-    """Initializes executor with a given engine.
-
-    Args:
-        engine: Initialized Engine object to operated on a given database.
-    """
-    self.engine = engine
+class SqlAlchemyQueryExecutor(sql_executor.SqlAlchemyQueryExecutor):
+  """Handles query execution via SqlAlchemy."""
 
   def execute(
     self,
@@ -63,17 +43,12 @@ class SqlAlchemyQueryExecutor(query_post_processor.PostProcessorMixin):
     Returns:
         DataFrame if query returns some data otherwise empty DataFrame.
     """
-    logging.info('Executing script: %s', script_name)
-    query_text = self.replace_params_template(query_text, params)
-    with self.engine.begin() as conn:
-      if re.findall(r'(create|update) ', query_text.lower()):
-        conn.connection.executescript(query_text)
-        return pd.DataFrame()
-      temp_table_name = f'temp_{script_name}'.replace('.', '_')
-      query_text = f'CREATE TABLE {temp_table_name} AS {query_text}'
-      conn.connection.executescript(query_text)
-      try:
-        result = pd.read_sql(f'SELECT * FROM {temp_table_name}', conn)
-      finally:
-        conn.connection.execute(f'DROP TABLE {temp_table_name}')
-      return result
+    context = (
+      execution_context.ExecutionContext(query_parameters=params)
+      if params
+      else execution_context.ExecutionContext()
+    )
+    report = super().execute(
+      query=query_text, title=script_name, context=context
+    )
+    return report.to_pandas()
