e857f00d15539cb32954a37841ac47e9647b164f
Andrei Markin <amarkin@google.com>
2026-02-04 13:03:00 +0400

    [gaarf-py] chore: handle positional arguments in deprecated classes
    Change-Id: Ic9362869270a35b5c44baeda987ae66e6d8a0ce1

--- py/gaarf/query_executor.py
+++ py/gaarf/query_executor.py
@@ -14,17 +14,27 @@
 
 # pylint: disable=C0330, g-bad-import-order, g-multiple-import
 
-"""Deprecated module for executing queries Gaarf queries."""
+"""Deprecated module for executing Gaarf queries."""
 
 from __future__ import annotations
 
+import warnings
+
 from gaarf import exceptions
+
+warnings.warn(
+  "The 'gaarf.query_executor' namespace is deprecated. "
+  "Please use 'gaarf.report_fetcher' or "
+  "'gaarf.executors.ads_executor` instead.",
+  DeprecationWarning,
+  stacklevel=2,
+)
 
 
 class AdsReportFetcher:
   """Deprecated class for creating AdsReportFetcher."""
 
-  def __init__(self, **kwargs: str) -> None:
+  def __init__(self, *args, **kwargs: str) -> None:
     raise exceptions.GaarfDeprecationError(
       'Loading AdsReportFetcher from `gaarf.query_executor` is '
       'deprecated; Import AdsReportFetcher from `gaarf.report_fetcher` '
@@ -35,7 +45,7 @@ class AdsReportFetcher:
 class AdsQueryExecutor:
   """Deprecated class for creating AdsQueryExecutor."""
 
-  def __init__(self, **kwargs: str) -> None:
+  def __init__(self, *args, **kwargs: str) -> None:
     raise exceptions.GaarfDeprecationError(
       'Loading AdsQueryExecutor from `gaarf.query_executor` is '
       'deprecated; Import AdsQueryExecutor from `gaarf.executors.ads_executor` '
