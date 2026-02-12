198c26dbc70b87ab29f47fc22effffe4c5fdd159
gurusai-voleti <gvoleti@google.com>
2026-02-12 13:44:05 +0000

    chore: update

--- gcp/create-gaarf-wf/build/index.js
+++ gcp/create-gaarf-wf/build/index.js
@@ -813,10 +813,10 @@ async function init() {
     const gcp_region = await askForGcpRegion(answers);
     // create a bucket if it doesn't exist
     // TODO: move this to setup.sh and just call it with create_bucket task
-    let res = await execCmd(`gsutil ls gs://${gcs_bucket}`, new clui.Spinner(`Checking if GCS bucket ${gcs_bucket} exists`), { silent: true });
+    let res = await execCmd(`gcloud storage ls gs://${gcs_bucket}`, new clui.Spinner(`Checking if GCS bucket ${gcs_bucket} exists`), { silent: true });
     if (res.code !== 0) {
         // bucket doesn't exist
-        res = await execCmd(`gsutil mb -l ${getMultiRegion(gcp_region)} -b on gs://${gcs_bucket}`, new clui.Spinner(`Creating a GCS bucket ${gcs_bucket}`), { silent: true });
+        res = await execCmd(`gcloud storage buckets create --location=${getMultiRegion(gcp_region)} --uniform-bucket-level-access gs://${gcs_bucket}`, new clui.Spinner(`Creating a GCS bucket ${gcs_bucket}`), { silent: true });
         if (res.code !== 0) {
             console.log(chalk.red(`Could not create a bucket ${gcs_bucket}. Most likely the installation will fail`));
             console.log(res.stderr || res.stdout);
@@ -831,12 +831,12 @@ async function init() {
             console.log(chalk.red(`Could not find script '${custom_ids_query_path}'`));
         }
         custom_query_gcs_path = `${gcs_base_path}/get-accounts.sql`;
-        deploy_custom_query_snippet = `gsutil -m cp ${custom_ids_query_path} $GCS_BASE_PATH/get-accounts.sql`;
+        deploy_custom_query_snippet = `gcloud storage cp ${custom_ids_query_path} $GCS_BASE_PATH/get-accounts.sql`;
     }
     let deploy_googleads_config_snippet = '';
     if (path_to_googleads_config) {
         deploy_googleads_config_snippet = `if [[  -f ${path_to_googleads_config} ]]; then
-  gsutil -m cp ${path_to_googleads_config} $GCS_BASE_PATH/google-ads.yaml
+  gcloud storage cp ${path_to_googleads_config} $GCS_BASE_PATH/google-ads.yaml
 fi`;
     }
     // Note that we deploy queries to hard-coded paths
@@ -846,14 +846,14 @@ GCS_BASE_PATH=${gcs_base_path}
 ${deploy_googleads_config_snippet}
 ${deploy_custom_query_snippet}
 
-gsutil -m rm -r $GCS_BASE_PATH/${path_to_ads_queries}
+gcloud storage rm --recursive $GCS_BASE_PATH/${path_to_ads_queries}
 if ls ./${path_to_ads_queries}/* 1> /dev/null 2>&1; then
-  gsutil -m cp -R ./${path_to_ads_queries}/* $GCS_BASE_PATH/${PATH_ADS_QUERIES}/
+  gcloud storage cp --recursive ./${path_to_ads_queries}/* $GCS_BASE_PATH/${PATH_ADS_QUERIES}/
 fi
 
-gsutil -m rm -r $GCS_BASE_PATH/${path_to_bq_queries}
+gcloud storage rm --recursive $GCS_BASE_PATH/${path_to_bq_queries}
 if ls ./${path_to_bq_queries}/* 1> /dev/null 2>&1; then
-  gsutil -m cp -R ./${path_to_bq_queries}/* $GCS_BASE_PATH/${PATH_BQ_QUERIES}/
+  gcloud storage cp --recursive ./${path_to_bq_queries}/* $GCS_BASE_PATH/${PATH_BQ_QUERIES}/
 fi
 `);
     // Create deploy-wf.sh
@@ -1084,18 +1084,18 @@ cd ..
         default: false,
     }, answers)).clone_dashboard) {
         await deployDashboard(answers, gcp_project_id, output_dataset, macro_bq);
-        await execCmd(`gsutil cp ${DASHBOARD_LINK_FILE} ${gcs_base_path}/`, new clui.Spinner(`Copying ${DASHBOARD_LINK_FILE} to GCS ${gcs_base_path}/`), { silent: true });
+        await execCmd(`gcloud storage cp ${DASHBOARD_LINK_FILE} ${gcs_base_path}/`, new clui.Spinner(`Copying ${DASHBOARD_LINK_FILE} to GCS ${gcs_base_path}/`), { silent: true });
     }
     // at last stage we'll copy all shell scripts to same GCS bucket in scrips folders, so another users could manage the project easily
-    await execCmd(`gsutil -m cp *.sh ${gcs_base_path}/scripts/;gsutil -m cp ${settings_file} ${gcs_base_path}/scripts/;gsutil -m cp ${wf_data_file} ${gcs_base_path}/scripts/`, new clui.Spinner(`Copying all shell scripts to GCS ${gcs_base_path}/scripts`), { silent: true });
+    await execCmd(`gcloud storage cp *.sh ${gcs_base_path}/scripts/;gcloud storage cp ${settings_file} ${gcs_base_path}/scripts/;gcloud storage cp ${wf_data_file} ${gcs_base_path}/scripts/`, new clui.Spinner(`Copying all shell scripts to GCS ${gcs_base_path}/scripts`), { silent: true });
     // create download-script.sh shell script to download scripts back from GCS
     deployShellScript('download-scripts.sh', `for file in *.sh; do
   [ -e "$file" ] || continue
   cp -- "$file" "$\{file}.bak"
 done
-gsutil -m cp ${gcs_base_path}/scripts/*.sh .
-gsutil -m cp ${gcs_base_path}/scripts/${settings_file} .
-gsutil -m cp ${gcs_base_path}/scripts/${wf_data_file} .
+gcloud storage cp ${gcs_base_path}/scripts/*.sh .
+gcloud storage cp ${gcs_base_path}/scripts/${settings_file} .
+gcloud storage cp ${gcs_base_path}/scripts/${wf_data_file} .
 `);
     console.log(`All generated shell scripts were uploaded to GCS ${chalk.cyan(gcs_base_path + '/scripts')}`);
     console.log(chalk.green('All done'));
